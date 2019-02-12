38fbc6bdfcec64d0ccb00b8e074a186d2ebcde15
Petr <petr.marecekk@gmail.com>
2019-02-12 11:12:40 +0100

    Add comparators to merge-sort algorithm

--- src/utils/component-helper.js
+++ src/utils/component-helper.js
@@ -1,58 +1,15 @@
 import { List } from 'immutable'
 
 export function mergeSort(list) {
-  if (list.size === 1) {
-    return list
-  }
-
-  let checkedNumberList = List()
-  let checkedDefaultList = List()
-  let numberList = List()
-  let defaultList = List()
-
-  for (const item of list) {
-    // checked items
-    if (item.isChecked) {
-      if (item.isNumber) {
-        checkedNumberList = checkedNumberList.push(item)
-        continue
-      }
-
-      checkedDefaultList = checkedDefaultList.push(item)
-      continue
-    }
-
-    // unchecked items
-    if (!item.isChecked) {
-      if (item.isNumber) {
-        numberList = numberList.push(item)
-        continue
-      }
-
-      defaultList = defaultList.push(item)
-      continue
-    }
-  }
-
-  // merge sort and concat result
-  return splitList(checkedNumberList)
-    .concat(splitList(checkedDefaultList))
-    .concat(splitList(numberList))
-    .concat(splitList(defaultList))
-}
-
-function splitList(list) {
   if (list.size <= 1) {
     return list
   }
 
-  // left and right side of list
   const middle = Math.floor(list.size / 2)
   const leftList = list.slice(0, middle)
   const rightList = list.slice(middle)
 
-  // merge left and right side of list
-  return merge(splitList(leftList), splitList(rightList))
+  return merge(mergeSort(leftList), mergeSort(rightList))
 }
 
 function merge(leftList, rightList) {
@@ -65,13 +22,7 @@ function merge(leftList, rightList) {
     const leftItem = leftList.get(indexLeft)
     const rightItem = rightList.get(indexRight)
 
-    // number type
-    if (leftItem.isNumber && rightItem.isNumber) {
-      leftItem.value = Number(leftItem.value)
-      rightItem.value = Number(rightItem.value)
-    }
-
-    if (leftItem.value < rightItem.value) {
+    if (comparator(leftItem, rightItem) <= 0) {
       leftItem.value = String(leftItem.value)
       result = result.push(leftItem)
       indexLeft++
@@ -84,4 +35,62 @@ function merge(leftList, rightList) {
 
   // concat sorting left and right side of list
   return result.concat(leftList.slice(indexLeft)).concat(rightList.slice(indexRight))
+}
+
+function comparatorChecked(leftItem, rightItem) {
+  if (leftItem.isChecked && rightItem.isChecked) {
+    const numberSorting = comparatorNumber(leftItem, rightItem)
+    const valueSorting = comparatorValue(leftItem, rightItem)
+
+    return numberSorting !== null ? numberSorting : valueSorting
+  }
+
+  if (rightItem.isChecked) {
+    return 1
+  }
+
+  if (leftItem.isChecked) {
+    return -1
+  }
+
+  return null
+}
+
+function comparatorNumber(leftItem, rightItem) {
+  if (leftItem.isNumber && rightItem.isNumber) {
+    leftItem.value = Number(leftItem.value)
+    rightItem.value = Number(rightItem.value)
+
+    return comparatorValue(leftItem, rightItem)
+  }
+
+  if (rightItem.isNumber) {
+    return 1
+  }
+
+  if (leftItem.isNumber) {
+    return -1
+  }
+
+  return null
+}
+
+function comparatorValue(leftItem, rightItem) {
+  return leftItem.value < rightItem.value
+    ? -1
+    : leftItem.value > rightItem.value
+    ? 1
+    : 0
+}
+
+function comparator(leftItem, rightItem) {
+  const checkedSorting = comparatorChecked(leftItem, rightItem)
+  const numberSorting = comparatorNumber(leftItem, rightItem)
+  const valueSorting = comparatorValue(leftItem, rightItem)
+
+  return checkedSorting !== null
+    ? checkedSorting
+    : numberSorting !== null
+    ? numberSorting
+    : valueSorting
 }
